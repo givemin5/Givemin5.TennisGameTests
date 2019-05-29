0a61f91df85202e23a7eb49e72f5cce91e51a0c1
ming <ming@anyo.com.tw>
2019-05-29 13:31:08 +0800

    確定環境

---
+++ Givemin5.TennisGameTests/TennisGameTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Givemin5.TennisGameTests
+{
+    [TestClass]
+    public class TennisGameTests
+    {
+        [TestMethod]
+        public void Love_All()
+        {
+            var tennisGame = new TennisGame();
+            Assert.AreEqual("Love All", tennisGame.Score());
+        }
+    }
+
+    public class TennisGame
+    {
+        public string Score()
+        {
+            return "Love All";
+        }
+    }
+}
