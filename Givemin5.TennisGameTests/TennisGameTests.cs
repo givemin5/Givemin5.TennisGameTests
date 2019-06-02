f1ac461463ba758c72877d8a5168fa0d5ef1b8a3
givemin5 <givemin5@gmail.com>
2019-06-02 17:06:24 +0800

    Fifteen Love Fail

--- Givemin5.TennisGameTests/TennisGameTests.cs
+++ Givemin5.TennisGameTests/TennisGameTests.cs
@@ -5,11 +5,22 @@ namespace Givemin5.TennisGameTests
     [TestClass]
     public class TennisGameTests
     {
+        readonly TennisGame _tennisGame = new TennisGame();
         [TestMethod]
         public void Love_All()
         {
-            var tennisGame = new TennisGame();
-            Assert.AreEqual("Love All", tennisGame.Score());
+            ScoreShoudBe("Love All");
+        }
+
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            _tennisGame.FirstPlayerScore();
+            ScoreShoudBe("Fifteen Love");
+        }
+        private void ScoreShoudBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennisGame.Score());
         }
     }
 }
