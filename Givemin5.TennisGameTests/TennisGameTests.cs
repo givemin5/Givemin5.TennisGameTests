2a252fd59ea24e746dd169c610ae8bdf2b8d6aff
givemin5 <givemin5@gmail.com>
2019-06-02 14:55:41 +0800

    Love All Fail

---
+++ Givemin5.TennisGameTests/TennisGameTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Givemin5.TennisGameTests
+{
+    [TestClass]
+    public class TennisGameTests
+    {
+        [TestMethod]
+        public void Love_All()
+        {
+            TennisGame tennisGame = new TennisGame();
+            Assert.AreEqual("Love All", tennisGame.Score());
+        }
+    }
+}
