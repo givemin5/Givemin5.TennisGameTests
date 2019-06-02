394fde00604b4b3d0a39c49b06dac4b4e92f6ab2
givemin5 <givemin5@gmail.com>
2019-06-02 17:13:21 +0800

    Love Thirty Fail

--- Givemin5.TennisGameTests/TennisGameTests.cs
+++ Givemin5.TennisGameTests/TennisGameTests.cs
@@ -37,8 +37,23 @@ namespace Givemin5.TennisGameTests
         [TestMethod]
         public void Love_Fifteen()
         {
-            _tennisGame.SecondPlayerScore();
+            SecondPlayerScoreTimes(1);
             ScoreShoudBe("Love Fifteen");
+        }
+
+        [TestMethod]
+        public void Love_Thirty()
+        {
+            SecondPlayerScoreTimes(2);
+            ScoreShoudBe("Love Thirty");
+        }
+
+        private void SecondPlayerScoreTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                _tennisGame.SecondPlayerScore();
+            }
         }
 
         private void FirstPlayerScoreTimes(int times)
