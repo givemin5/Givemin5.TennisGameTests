2eb70b684d34108f91ce7ccba0db344496bc8c10
givemin5 <givemin5@gmail.com>
2019-06-10 23:30:31 +0800

    增加 cc 與 forloop

--- Givemin5.TennisGameTests/SourceTemplates.cs
+++ Givemin5.TennisGameTests/SourceTemplates.cs
@@ -3,18 +3,35 @@ namespace Givemin5.TennisGameTests
 {
     using JetBrains.Annotations;
     public static class SourceTemplates
+    {
+        [SourceTemplate]
+        [Macro(Target = "expected", Editable = 1)]
+        public static void ae<T>(this T source)
         {
-            [SourceTemplate]
-            [Macro(Target = "expected", Editable = 1)]
-            public static void ae<T>(this T source)
-            {
-                /*$ Assert.AreEqual( $expected$ , source);*/
-            }
-
-            [SourceTemplate]
-            public static void abs<T>(this T source)
-            {
-                /*$ Math.Abs(source)*/
-            }
+            /*$ Assert.AreEqual( $expected$ , source);*/
         }
+
+        [SourceTemplate]
+        public static void abs<T>(this T source)
+        {
+            /*$ Math.Abs(source)*/
+        }
+        [SourceTemplate]
+        public static void cc<T>(this T source)
+        {
+            /*$ source;
+                source
+             */
+        }
+        [SourceTemplate]
+        [Macro(Target = "parameter", Editable = 1)]
+        public static void forloop<T>(this T source)
+        {
+            /*$ for (int i = 0; i< $parameter$; i++)
+            {
+                source;
+            }
+            */
+        }
+    }
 }
