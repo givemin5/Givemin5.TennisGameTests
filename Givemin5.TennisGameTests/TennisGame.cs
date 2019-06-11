24b23957f3ee8992471aaa88d68c80775fb9764e
givemin5 <givemin5@gmail.com>
2019-06-12 00:11:05 +0800

    Thirty Love P

--- Givemin5.TennisGameTests/TennisGame.cs
+++ Givemin5.TennisGameTests/TennisGame.cs
@@ -10,7 +10,10 @@
             {
                 return "Fifteen Love";
             }
-
+            if (_firstPlayerScoreTimes == 2)
+            {
+                return "Thirty Love";
+            }
             return "Love All";
         }
 
