b423e6fec2ef9421cdafc860d507af9635c30799
givemin5 <givemin5@gmail.com>
2019-06-12 00:19:12 +0800

    Fifteen Thiry P

--- Givemin5.TennisGameTests/TennisGame.cs
+++ Givemin5.TennisGameTests/TennisGame.cs
@@ -11,17 +11,14 @@ namespace Givemin5.TennisGameTests
         {
             Dictionary<int, string> scoreLookup = new Dictionary<int, string>
             {
+                {0,"Love" },
                 {1,"Fifteen" },
                 {2,"Thirty" },
                 {3,"Forty" },
             };
-            if (_firstPlayerScoreTimes >= 1)
+            if (_firstPlayerScoreTimes >= 1 || _secondPlayerScoreTimes >= 1)
             {
-                return $"{scoreLookup[_firstPlayerScoreTimes]} Love";
-            }
-            if (_secondPlayerScoreTimes >= 1)
-            {
-                return $"Love {scoreLookup[_secondPlayerScoreTimes]}";
+                return $"{scoreLookup[_firstPlayerScoreTimes]} {scoreLookup[_secondPlayerScoreTimes]}";
             }
             return "Love All";
         }
