06e7f81a06c7fa077d323c8913f1b62c197528f5
givemin5 <givemin5@gmail.com>
2019-06-19 23:21:02 +0800

    17:28

---
+++ Givemin5.TennisGameTests/TennisGame.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Givemin5.TennisGameTests
+{
+    public class TennisGame
+    {
+        private readonly string _firstPlayerName;
+        private readonly string _secondPlayerName;
+        private int _firstPlayerScoreTimes;
+
+        private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            {0,"Love" },
+            {1,"Fifteen" },
+            {2,"Thirty" },
+            {3,"Forty" },
+        };
+
+        private int _secondPlayerScoreTimes;
+
+        public TennisGame(string firstPlayerName, string secondPlayerName)
+        {
+            _firstPlayerName = firstPlayerName;
+            _secondPlayerName = secondPlayerName;
+        }
+
+        public string Score()
+        {
+            if (IsScoreDifferent())
+            {
+                if (IsReadyForWin())
+                {
+                    var advPlayer = AdvPlayer();
+                    if (IsAdv())
+                    {
+                        return $"{advPlayer} Adv";
+                    }
+                    return $"{advPlayer} Win";
+                }
+
+                return $"{_scoreLookup[_firstPlayerScoreTimes]} {_scoreLookup[_secondPlayerScoreTimes]}";
+            }
+            if (IsDeuce())
+            {
+                return "Deuce";
+            }
+            return $"{_scoreLookup[_firstPlayerScoreTimes]} All";
+        }
+
+        private bool IsScoreDifferent()
+        {
+            return _firstPlayerScoreTimes != _secondPlayerScoreTimes;
+        }
+
+        private bool IsAdv()
+        {
+            return Math.Abs(_firstPlayerScoreTimes - _secondPlayerScoreTimes) == 1;
+        }
+
+        private bool IsReadyForWin()
+        {
+            return _firstPlayerScoreTimes > 3 || _secondPlayerScoreTimes > 3;
+        }
+
+        private bool IsDeuce()
+        {
+            return _firstPlayerScoreTimes >= 3;
+        }
+
+        private string AdvPlayer()
+        {
+            var advPlayer = _firstPlayerScoreTimes > _secondPlayerScoreTimes
+                ? _firstPlayerName
+                : _secondPlayerName;
+            return advPlayer;
+        }
+
+        public void FirstPlayerScore()
+        {
+            _firstPlayerScoreTimes++;
+        }
+
+        public void SecondPlayerScore()
+        {
+            _secondPlayerScoreTimes++;
+        }
+    }
+}
